f8f25445928475783b117f200ec3320ebd1ad323
Tusita <isabel@MacBook-Pro-de-isabel.local>
2019-10-21 19:49:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/isabel/fragment/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.isabel.fragment;
+
+import android.os.Bundle;
+        import android.support.v4.app.FragmentManager;
+        import android.support.v4.app.FragmentTransaction;
+        import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements
+        FragmentOne.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FragmentOne fragment = FragmentOne.newInstance();
+        fragmentTransaction.add(R.id.fragmentOne, fragment);
+        fragmentTransaction.commit();
+    }
+
+
+
+    @Override
+    public void onFragmentInteraction(String text, int colorId) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FragmentTwo fragment = FragmentTwo.newInstance(text, colorId);
+        fragmentTransaction.replace(R.id.fragmentTwo, fragment);
+        fragmentTransaction.commit();
+    }
+}
